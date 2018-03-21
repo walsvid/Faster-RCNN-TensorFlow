720f146b3ac212a40fb6ce4e44ab189e5357b6d0
walsvid <walsvid@gmail.com>
2018-03-21 21:03:42 +0800

    walsvid - convert other lib files to py3

--- lib/rpn_msr/proposal_layer.py
+++ lib/rpn_msr/proposal_layer.py
@@ -7,10 +7,10 @@
 
 import numpy as np
 import yaml
-from fast_rcnn.config import cfg
-from generate_anchors import generate_anchors
-from fast_rcnn.bbox_transform import bbox_transform_inv, clip_boxes
-from fast_rcnn.nms_wrapper import nms
+from lib.fast_rcnn.config import cfg
+from lib.rpn_msr.generate_anchors import generate_anchors
+from lib.fast_rcnn.bbox_transform import bbox_transform_inv, clip_boxes
+from lib.fast_rcnn.nms_wrapper import nms
 import pdb
 
 
@@ -57,14 +57,14 @@ def proposal_layer(rpn_cls_prob_reshape,rpn_bbox_pred,im_info,cfg_key,_feat_stri
     #im_info = bottom[2].data[0, :]
 
     if DEBUG:
-        print 'im_size: ({}, {})'.format(im_info[0], im_info[1])
-        print 'scale: {}'.format(im_info[2])
+        print('im_size: ({}, {})'.format(im_info[0], im_info[1]))
+        print('scale: {}'.format(im_info[2]))
 
     # 1. Generate proposals from bbox deltas and shifted anchors
     height, width = scores.shape[-2:]
 
     if DEBUG:
-        print 'score map size: {}'.format(scores.shape)
+        print('score map size: {}'.format(scores.shape))
 
     # Enumerate all shifts
     shift_x = np.arange(0, width) * _feat_stride
