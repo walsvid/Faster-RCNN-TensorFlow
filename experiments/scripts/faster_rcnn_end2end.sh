4fccceb272edc5296c13f0ad2cc6eb2f742fd1a2
walsvid <walsvid@gmail.com>
2018-03-24 21:34:18 +0800

    walsvid - update exp

--- experiments/scripts/faster_rcnn_end2end.sh
+++ experiments/scripts/faster_rcnn_end2end.sh
@@ -21,7 +21,7 @@ case $DATASET in
     TRAIN_IMDB="voc_2007_trainval"
     TEST_IMDB="voc_2007_test"
     PT_DIR="pascal_voc"
-    ITERS=100
+    ITERS=70000
     ;;
   coco)
     # This is a very long and slow training schedule
