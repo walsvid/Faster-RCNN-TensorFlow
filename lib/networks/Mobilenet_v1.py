1c8dd7c6629212a572b3dfc9c585cd7b2278e7d5
walsvid <walsvid@gmail.com>
2018-04-02 23:08:56 +0800

    walsvid - this commit closes #5

--- lib/networks/Mobilenet_v1.py
+++ lib/networks/Mobilenet_v1.py
@@ -20,6 +20,7 @@ class MobilenetV1(Network):
         self.layers = dict({'data': self.data, 'im_info': self.im_info, 'gt_boxes': self.gt_boxes})
 
     def setup(self):
+        # ========= HeadNet ============
         (self.feed('data')
          .conv(3, 3, 32, 2, 2, biased=False, relu=False, name='conv1')
          .batch_normalization(relu=True, name='conv1_bn')
@@ -74,7 +75,47 @@ class MobilenetV1(Network):
          .conv(3, 3, 1024, 1, 1, biased=False, group=1024, relu=False, name='conv6_dw')
          .batch_normalization(relu=True, name='conv6_dw_bn')
          .conv(1, 1, 1024, 1, 1, biased=False, relu=False, name='conv6_sep')
-         .batch_normalization(relu=True, name='conv6_sep_bn')
-         .avg_pool(7, 7, 1, 1, padding='VALID', name='pool6')
+         .batch_normalization(relu=True, name='conv6_sep_bn'))
+
+        # ========= RPN ============
+        (self.feed('conv6_sep_bn')
+         .conv(3, 3, 512, 1, 1, name='rpn_conv/3x3')
+         .conv(1, 1, len(self.anchor_scales) * 3 * 2, 1, 1, padding='VALID', relu=False, name='rpn_cls_score'))
+
+        if self.is_train:
+            (self.feed('rpn_cls_score', 'gt_boxes', 'im_info', 'data')
+             .anchor_target_layer(self.feat_stride, self.anchor_scales, name='rpn-data'))
+            # Loss of rpn_cls & rpn_boxes
+
+        (self.feed('rpn_conv/3x3')
+         .conv(1, 1, len(self.anchor_scales) * 3 * 4, 1, 1, padding='VALID', relu=False, name='rpn_bbox_pred'))
+
+        # ========= RoI Proposal ============
+        (self.feed('rpn_cls_score')
+         .spatial_reshape_layer(2, name='rpn_cls_score_reshape')
+         .spatial_softmax(name='rpn_cls_prob'))
+
+        (self.feed('rpn_cls_prob')
+         .spatial_reshape_layer(len(self.anchor_scales) * 3 * 2, name='rpn_cls_prob_reshape'))
+
+        (self.feed('rpn_cls_prob_reshape', 'rpn_bbox_pred', 'im_info')
+         .proposal_layer(self.feat_stride, self.anchor_scales, self.mode, name=self.proposal_layer_name))
+
+        if self.is_train:
+            (self.feed('rpn_rois', 'gt_boxes')
+             .proposal_target_layer(self.n_classes, name='roi-data'))
+
+        feed_layer = 'roi-data' if self.is_train else 'rois'
+        (self.feed('conv6_sep_bn', feed_layer)
+         .roi_pool(7, 7, 1.0 / 16, name='roi_pool')
          .conv(1, 1, 1000, 1, 1, relu=False, name='fc7')
-         .softmax(name='prob'))
+         .conv(1, 1, self.n_classes, 1, 1, relu=False, name='cls_score')
+         .softmax(name='cls_prob'))
+
+        (self.feed('fc7')
+         .conv(1, 1, self.n_classes * 4, 1, 1, relu=False, name='bbox_pred'))
+
+        if self.is_train:
+            self.bbox_normalization()
+
+        return self
