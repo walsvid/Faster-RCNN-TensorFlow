aa032010f8a4a96038c900b68a0849666e2fb687
walsvid <walsvid@gmail.com>
2018-04-02 22:16:37 +0800

    walsvid - remove useless import

--- lib/networks/factory.py
+++ lib/networks/factory.py
@@ -1,18 +1,8 @@
-# --------------------------------------------------------
-# SubCNN_TF
-# Copyright (c) 2016 CVGL Stanford
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Yu Xiang
-# --------------------------------------------------------
-
 """Factory method for easily getting imdbs by name."""
 
 import networks.VGG16
 import networks.Resnet50
 import networks.Resnet101
-import pdb
-import tensorflow as tf
-
 
 __sets = {}
 
